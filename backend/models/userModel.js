0e24c739be2691ed88f9e6292412899d82f0e524
ankit <saharan442090@gmail.com>
2025-08-31 18:26:20 +0530

    create user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profilePhoto: {
+    type: String,
+    default: ""
+  },
+  gender: {
+    type: String,
+    enum: ["male","female"],
+    required: true
+  }
+}, {timestamps: true})
+
+const UserModel = mongoose.model("User",userSchema)
+
+module.exports = UserModel
