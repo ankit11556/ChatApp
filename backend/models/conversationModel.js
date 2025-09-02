dd0756df3994079847c5474ed1d7006711e7e016
ankit <saharan442090@gmail.com>
2025-09-02 20:54:58 +0530

    create conversation model

---
+++ backend/models/conversationModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const conversationSchema = new mongoose.Schema({
+  participants: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  messages: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Message"
+  }]
+})
+
+const ConversationModel = mongoose.model('Conversation',conversationSchema)
+
+module.exports = ConversationModel
