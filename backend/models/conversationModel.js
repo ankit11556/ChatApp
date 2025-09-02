da23da0a11fe3de2d3b90f826c06d191285501d7
ankit <saharan442090@gmail.com>
2025-09-02 21:09:46 +0530

    add timestamps in conversation model

--- backend/models/conversationModel.js
+++ backend/models/conversationModel.js
@@ -9,7 +9,7 @@ const conversationSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Message"
   }]
-})
+},{timestamps:true})
 
 const ConversationModel = mongoose.model('Conversation',conversationSchema)
 
